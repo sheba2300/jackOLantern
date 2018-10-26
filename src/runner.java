e2dcca6e9fbc04db88660214b6e651ff9aec4405
sheba sow <ssow8171@bths.edu>
2018-10-26 10:17:18 -0400

    making face

--- src/runner.java
+++ src/runner.java
@@ -1,5 +1,33 @@
 public class runner {
-    String[][] face = new String[4][4];
-    jackOLantern jack = new jackOLantern(face);
-   jack.fill("~");
+    public static void main(String args[])
+    {
+        String[][] face = new String[4][5];
+        jackOLantern jack = new jackOLantern(face);
+        jack.fill(" ");
+        jack.edit("|", 1,0);
+        jack.edit("|", 2,0);
+        jack.edit("|", 3,0);
+        jack.edit("|", 1,4);
+        jack.edit("|", 2,4);
+        jack.edit("|", 3,4);
+        jack.edit("~", 0,0);
+        jack.edit("~", 0,1);
+        jack.edit("~", 0,2);
+        jack.edit("~", 0,3);
+        jack.edit("0", 1,1);
+        jack.edit("~", 1,3);
+        jack.edit("~", 2,1);
+        jack.edit("~", 0,0);
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
